2c80f1386ff957b3fa0faf77a1f9bd3dc4c07490
jeonggeonwoong <ghtm543@gmail.com>
2023-05-31 17:05:26 +0900

    change config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://geon-woong.github.io/realworld-front/",
+  base:"https://realworld-woong.vercel.app/",
 })
 
